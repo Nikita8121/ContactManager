ccdb357bbd36cbd35338917927d936b39616a940
Nikita8121 <natanalegarh@gmail.com>
2022-12-24 13:18:26 +0200

    Add project files.

---
+++ ContactManager/Models/Call.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    public class Call
+    {
+        public DateTime callDate;
+    }
+}
---
+++ ContactManager/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    public class User
+    {
+        public string Name { get; }
+        public string Email { get; }
+        public string PhoneNumber { get; }
+        private List<Call> _callsHistory;
+
+        public User (string name, string email, string phoneNumber)
+        {
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            _callsHistory = new List<Call>();
+        }
+
+        public void AddCall(Call call)
+        {
+            _callsHistory.Add(call);
+        }
+    }
+}
